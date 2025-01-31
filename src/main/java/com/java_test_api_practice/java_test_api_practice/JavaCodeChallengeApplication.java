1da1b106c42f55cba4958ae8f76ae8798bfb0094
frc90 <39072359+frc90@users.noreply.github.com>
2025-01-31 12:38:21 -0300

    Initial Project: Code Challenge

---
+++ src/main/java/com/java_test_api_practice/java_test_api_practice/JavaCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.java_test_api_practice.java_test_api_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_test_api_practice/java_test_api_practice/JavaCodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_test_api_practice.java_test_api_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
